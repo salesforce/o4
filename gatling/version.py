58860fb549d1f935c2acf5ce9bc7869b4d4c6d41
Philip Bergen <pbergen@salesforce.com>
2018-10-12 17:37:38 -0400

    Version bump.

--- gatling/version.py
+++ gatling/version.py
@@ -3,13 +3,13 @@
 
 import datetime.datetime
 
-VERSION = (1, 1, 8)
-VERSION_STR = '1.1.8'
+VERSION = (1, 1, 9)
+VERSION_STR = '1.1.9'
 PRODUCT = 'gatling/version.py'
 
 REQ_MD5 = '66d2b20c6fa5c95ac17a05d908889c66'
-PY_MD5 = '9e40302af3bb7ab0d58e9e504af10609'
+PY_MD5 = 'aed10058c353c0b8600bbc9360fb60d0'
 
-TIMESTAMP = datetime.datetime(2018, 10, 3, 17, 10, 19, 471410)
+TIMESTAMP = datetime.datetime(2018, 10, 12, 17, 36, 14, 61066)
 USER_NAME = 'Philip Bergen'
 USER_EMAIL = 'pbergen@salesforce.com'

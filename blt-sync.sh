998e33af2ee6e922da034054cab4b7512451ad44
Philip Bergen <pbergen@salesforce.com>
2018-10-12 17:36:45 -0400

    Merged in latest changes.

---
+++ blt-sync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu
+
+##
+# copy changes over from ~/blt/blt-code/plugins/perforce
+
+for dir in o4 server gatling; do
+    cd $dir
+    for fname in *.py requirements.txt; do
+        blt_name=$fname
+        [ $fname = o4.py ] && blt_name=o4_sync.py
+        blt_name=~/blt/blt-code/plugins/perforce/$blt_name
+        [ -f $fname -a -f $blt_name ] || continue
+        diff -du $fname $blt_name && diff=false || diff=true
+        if [ $diff = true ]; then
+            echo -en "[$dir/$fname] \tDo you want to overwrite with changes from $blt_name? [Yn] "
+            read go
+            if [[ "$go" != n && "$go" != N ]]; then
+                cp -a $blt_name $fname
+                git diff $fname
+                echo -n "Press [ENTER] to continue"
+            fi
+        fi
+    done
+    cd ..
+done

ca571e6142c37997890de72bd2a1d27786c2fe65
Philip Bergen <pbergen@salesforce.com>
2018-10-23 00:24:01 -0400

    Version bump.

--- o4/version.py
+++ o4/version.py
@@ -1,14 +1,15 @@
+
 # This file is generated, do not modify
 
 import datetime.datetime
 
-VERSION = (1, 2, 0)
-VERSION_STR = '1.2.0'
+VERSION = (1, 2, 1)
+VERSION_STR = '1.2.1'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '0521206afbb40a293c10995f2a03994b'
+PY_MD5 = '1cb3b19c6b35e46a20b631f1f56ab525'
 
-TIMESTAMP = datetime.datetime(2018, 10, 12, 17, 36, 10, 136512)
+TIMESTAMP = datetime.datetime(2018, 10, 23, 0, 23, 29, 653219)
 USER_NAME = 'Philip Bergen'
 USER_EMAIL = 'pbergen@salesforce.com'

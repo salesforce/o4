917eee4b74bc6e817fcb7f1acb8761844e1c85cf
Clarence <clarence.gardner@salesforce.com>
2019-02-28 21:33:22 -0500

    Build for change to inexact changelist messaGE.

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime.datetime
 
-VERSION = (1, 2, 3)
-VERSION_STR = '1.2.3'
+VERSION = (1, 2, 4)
+VERSION_STR = '1.2.4'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = 'db68ff3e2921525c53b2ff399f3882e9'
+PY_MD5 = 'cd0fe57c5f312f22e8f8ea8b0c43f73a'
 
-TIMESTAMP = datetime.datetime(2019, 1, 11, 16, 51, 43, 816447)
-USER_NAME = 'Philip Bergen'
-USER_EMAIL = 'pbergen@salesforce.com'
+TIMESTAMP = datetime.datetime(2019, 2, 28, 20, 38, 22, 999884)
+USER_NAME = 'Clarence'
+USER_EMAIL = 'clarence.gardner@salesforce.com'

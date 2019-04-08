d3533e6bfec6b15845292fd3a1ccb5a412c8525f
Bhargav Medha Prabhala <mprabhala@salesforce.com>
2019-04-08 16:12:33 -0700

    decode bytes to str

--- gatling/version.py
+++ gatling/version.py
@@ -3,13 +3,13 @@
 
 import datetime.datetime
 
-VERSION = (1, 1, 9)
-VERSION_STR = '1.1.9'
+VERSION = (1, 1, 10)
+VERSION_STR = '1.1.10'
 PRODUCT = 'gatling/version.py'
 
 REQ_MD5 = '66d2b20c6fa5c95ac17a05d908889c66'
-PY_MD5 = 'aed10058c353c0b8600bbc9360fb60d0'
+PY_MD5 = 'b5913ae81d2b6fb11cc4382c93c79770'
 
-TIMESTAMP = datetime.datetime(2019, 1, 11, 16, 51, 46, 155279)
-USER_NAME = 'Philip Bergen'
-USER_EMAIL = 'pbergen@salesforce.com'
+TIMESTAMP = datetime.datetime(2019, 4, 8, 16, 1, 7, 856969)
+USER_NAME = 'Bhargav Medha Prabhala'
+USER_EMAIL = 'mprabhala@salesforce.com'
--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime.datetime
 
-VERSION = (1, 2, 5)
-VERSION_STR = '1.2.5'
+VERSION = (1, 2, 6)
+VERSION_STR = '1.2.6'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '4e3078c7a425b0e2a76b9eb830fa2c82'
+PY_MD5 = '12cffd3e3acdb418042a10abe8d307f9'
 
-TIMESTAMP = datetime.datetime(2019, 4, 3, 15, 20, 26, 504013)
-USER_NAME = 'Clarence'
-USER_EMAIL = 'clarence.gardner@salesforce.com'
+TIMESTAMP = datetime.datetime(2019, 4, 8, 14, 43, 3, 442738)
+USER_NAME = 'Bhargav Medha Prabhala'
+USER_EMAIL = 'mprabhala@salesforce.com'

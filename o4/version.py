a1f245347fc6790b2eeba8ab0ab31ffeb023099a
ClarenceGardner <clarence@silcom.com>
2019-06-25 16:31:05 -0400

    Noreplace (#16)
    * Pass through some "warning" results from perforce, to be reported by o4_fail.
    
    * Do not leave a "changelist" file if there were files unsynced with warnings.
    
    * Review comments.
    
    * Wording
    
    * Make it clear that the workspace is not correct.
    
    * Better process list.

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime.datetime
 
-VERSION = (1, 2, 47)
-VERSION_STR = '1.2.47'
+VERSION = (1, 2, 67)
+VERSION_STR = '1.2.67'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = 'a06ef5c0f421b18219aba0fc6ac866fc'
+PY_MD5 = '1dca473aaa19d0fb03bf7d9254885d74'
 
-TIMESTAMP = datetime.datetime(2019, 6, 5, 17, 44, 28, 824665)
+TIMESTAMP = datetime.datetime(2019, 6, 13, 11, 19, 12, 848553)
 USER_NAME = 'Clarence'
 USER_EMAIL = 'clarence.gardner@salesforce.com'

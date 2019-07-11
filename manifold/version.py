9e4157658f60136643863ad37dace5577b647001
Philip Bergen <pbergen@salesforce.com>
2019-07-11 11:48:43 -0400

    Version bump

--- manifold/version.py
+++ manifold/version.py
@@ -10,6 +10,6 @@ PRODUCT = 'manifold/version.py'
 REQ_MD5 = '66d2b20c6fa5c95ac17a05d908889c66'
 PY_MD5 = '0c440f1a119cffbf294df9104d4cc0a3'
 
-TIMESTAMP = datetime.datetime(2019, 4, 8, 16, 1, 8, 920678)
-USER_NAME = 'Bhargav Medha Prabhala'
-USER_EMAIL = 'mprabhala@salesforce.com'
+TIMESTAMP = datetime.datetime(2019, 7, 11, 11, 43, 44, 418839)
+USER_NAME = 'Philip Bergen'
+USER_EMAIL = 'pbergen@salesforce.com'
--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime.datetime
 
-VERSION = (1, 2, 67)
-VERSION_STR = '1.2.67'
+VERSION = (1, 2, 68)
+VERSION_STR = '1.2.68'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '1dca473aaa19d0fb03bf7d9254885d74'
+PY_MD5 = '315e80e8056f35873aca54b51a930b1f'
 
-TIMESTAMP = datetime.datetime(2019, 6, 13, 11, 19, 12, 848553)
-USER_NAME = 'Clarence'
-USER_EMAIL = 'clarence.gardner@salesforce.com'
+TIMESTAMP = datetime.datetime(2019, 7, 11, 11, 43, 39, 767743)
+USER_NAME = 'Philip Bergen'
+USER_EMAIL = 'pbergen@salesforce.com'

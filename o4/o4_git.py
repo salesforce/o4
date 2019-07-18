c4633d24ee013fa6b03283868e75dda16f6d3b9a
Philip Bergen <pbergen@salesforce.com>
2019-07-18 12:29:43 -0400

    Use ls-files to find and add untracked files with lfs support.

--- o4/o4_git.py
+++ o4/o4_git.py
@@ -72,69 +72,78 @@ def git_o4_import(prep_res):
         with open('.p4_original_cl', 'wt') as fout:
             fout.write(f"{prep_res['target_changelist']}")
         err_check_call(['git', 'add', '.p4_original_cl'])
-    adding, updating, removing = [], [], []
-    for i, fs in enumerate(
-            fstat_from_csv(f".o4/{prep_res['target_changelist']}.fstat.gz", fstat_split)):
-        if not fs:
-            continue
-        if int(fs[F_CHANGELIST]) <= prep_res['old_changelist']:
-            if i:
-                err_print(" OK")
-            break
-        token = '.'
-        if not fs[F_CHECKSUM]:
-            token = 'd'
-            removing.append(fs[F_PATH])
-        elif fs[F_REVISION] == '1':
-            token = 'a'
-            adding.append(fs[F_PATH])
-        else:
-            updating.append(fs[F_PATH])
-        # TODO: Check if renames screw up or git figures it out
-        err_print(token, end='')
-        if i and not (i % 50):
-            err_print(f" {i}")
-        sys.stderr.flush()
-    if adding or updating or removing:
-        chunk = 200
+    res = check_output(['git', 'ls-files', '--others', '--exclude-standard'],
+                       universal_newlines=True)
+    if res.returncode:
+        sys.exit(f"*** ERROR: Git could not present untracked files.")
+    untracked = set(res.stdout.strip().splitlines())
+    if untracked:
+        adding, lfs = [], []
+        for i, fs in enumerate(
+                fstat_from_csv(f".o4/{prep_res['target_changelist']}.fstat.gz", fstat_split)):
+            if not fs:
+                continue
+            if fs[F_PATH] in untracked and fs[F_CHECKSUM]:
+                if os.lstat(fs[F_PATH]).st_size > 1e6:
+                    lfs.append(fs[F_PATH])
+                else:
+                    adding.append(fs[F_PATH])
+                untracked.remove(fs[F_PATH])
+                if not untracked:
+                    break
+        with open('.o4/adding.txt', 'wt') as fout:
+            for a in adding:
+                print(a, file=fout)
+        with open('.o4/lfs.txt', 'wt') as fout:
+            for a in lfs:
+                print(a, file=fout)
+        err_print(f"*** INFO: Adding {len(adding)} new files, and "
+                  f"{len(lfs)} new large files, to git.")
+        if lfs:
+            err_print("*** INFO: Tracking LFS files...")
+            err_check_call(['git', 'lfs', 'track'] + lfs)
+            adding.extend(lfs)
         if adding:
             err_print(f"*** INFO: Adding {len(adding)} new files to git...", end=' ')
             while adding:
                 err_print(len(adding), end=' ')
                 sys.stderr.flush()
-                err_check_call(['git', 'add'] + adding[:chunk])
-                del adding[:chunk]
+                err_check_call(['git', 'add'] + adding[:200])
+                del adding[:200]
             err_print('')
-        if updating:
-            err_print(f"*** INFO: Updating {len(updating)} updated files in git...")
-            err_check_call(['git', 'add', '-u'])
-        if removing:
-            err_print(f"*** INFO: Removing {len(adding)} deleted files from git...", end=' ')
-            while removing:
-                err_print(len(adding), end=' ')
-                sys.stderr.flush()
-                err_check_call(['git', 'rm', '-f', '--ignore-unmatch'] + removing[:chunk])
-                del removing[:chunk]
-            err_print('')
-        err_print(f"*** INFO: Committing changes from o4 to git {prep_res['merge_target']}...")
-        res = run([
-            'git', 'commit', '-m',
-            (f"o4 git import {prep_res['depot_path']} from CL {prep_res['old_changelist']} to"
-             f" {prep_res['target_changelist']}")
-        ],
-                  universal_newlines=True,
-                  stdout=PIPE,
-                  stderr=STDOUT)
-        err_print(f"*** {'WARNING' if res.returncode else 'INFO'}: Git response:",
-                  res.stdout.replace("\n", "\n    "))
-        if res.returncode and 'nothing to commit' not in res.stdout:
-            sys.exit("*** ERROR: Failed to commit imported changes to git.")
+        if untracked:
+            err_print(f"*** WARNING: There are {len(untracked)} untracked files not covered by o4:")
+            for u in untracked:
+                err_print("   ", u)
+            err_print("*** WARNING: The above listed files will be addeded to .gitignore")
+            with open('.gitignore', 'at+') as fout:
+                for u in sorted(untracked):
+                    print(u, file=fout)
+            err_check_call(['git', 'add', '.gitignore'])
+    err_print(f"*** INFO: Updating affected files in git...")
+    err_check_call(['git', 'add', '-u'])
+    err_print(f"*** INFO: Committing changes from o4 to git {prep_res['merge_target']}...")
+    res = run([
+        'git', 'commit', '-m',
+        (f"o4 git import {prep_res['depot_path']} from CL {prep_res['old_changelist']} to"
+         f" {prep_res['target_changelist']}")
+    ],
+              universal_newlines=True,
+              stdout=PIPE,
+              stderr=STDOUT)
+    err_print(f"*** {'WARNING' if res.returncode else 'INFO'}: Git response:",
+              res.stdout.replace("\n", "\n    "))
+    if res.returncode and 'nothing to commit' not in res.stdout:
+        sys.exit("*** ERROR: Failed to commit imported changes to git.")
 
 
 def git_master_restore(prep_res):
     if git_current_branch() != prep_res['topic']:
         err_print(f"*** INFO: Returning git to topic branch {prep_res['topic']}...")
         err_check_call(['git', 'checkout', prep_res['topic']])
+        err_print(f"*** INFO: Merging {prep_res['merge_target']} into "
+                  f"topic branch {prep_res['topic']}...")
+        err_check_call(['git', 'merge', prep_res['merge_target']])
 
     if ':' in prep_res['stash']:
         err_print('*** INFO: Popping git stash...')
--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime.datetime
 
-VERSION = (1, 2, 74)
-VERSION_STR = '1.2.74'
+VERSION = (1, 2, 79)
+VERSION_STR = '1.2.79'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '135b61c1b8c46ccf0d378a64e440011f'
+PY_MD5 = 'b334395af48d3e56d81cf16eadb7ca5e'
 
-TIMESTAMP = datetime.datetime(2019, 7, 15, 13, 0, 15, 455177)
+TIMESTAMP = datetime.datetime(2019, 7, 18, 12, 27, 59, 836437)
 USER_NAME = 'Philip Bergen'
 USER_EMAIL = 'pbergen@salesforce.com'

8117a64986077afda3dcd2fe7ef987487d0aadcf
Philip Bergen <pbergen@salesforce.com>
2019-09-17 16:29:21 -0400

    Fix broken syntax in version.py.

--- manifold/version.py
+++ manifold/version.py
@@ -1,15 +1,15 @@
 
 # This file is generated, do not modify
 
-import datetime.datetime
+import datetime
 
-VERSION = (1, 1, 11)
-VERSION_STR = '1.1.11'
+VERSION = (1, 1, 12)
+VERSION_STR = '1.1.12'
 PRODUCT = 'manifold/version.py'
 
 REQ_MD5 = '66d2b20c6fa5c95ac17a05d908889c66'
-PY_MD5 = '0c440f1a119cffbf294df9104d4cc0a3'
+PY_MD5 = '9c9a8211f2a173262a01841f4bbb37b5'
 
-TIMESTAMP = datetime.datetime(2019, 7, 11, 11, 43, 44, 418839)
+TIMESTAMP = datetime.datetime(2019, 9, 17, 16, 27, 35, 877431)
 USER_NAME = 'Philip Bergen'
 USER_EMAIL = 'pbergen@salesforce.com'

6f4244c0452ecf3c30cff5ed62b7a57f9588ff17
Clarence <clarence.gardner@salesforce.com>
2019-11-06 21:18:54 -0500

    No newline literal in f-string.

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 2, 73)
-VERSION_STR = '1.2.73'
+VERSION = (1, 2, 135)
+VERSION_STR = '1.2.135'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '29cbb81f40788ba71d71d5fa9bd8cf2e'
+PY_MD5 = 'db8f6f20dc1a0a1c131b83cbad84f205'
 
-TIMESTAMP = datetime.datetime(2019, 10, 28, 19, 25, 44, 146189)
+TIMESTAMP = datetime.datetime(2019, 11, 5, 13, 31, 17, 599593)
 USER_NAME = 'Clarence'
 USER_EMAIL = 'clarence.gardner@salesforce.com'

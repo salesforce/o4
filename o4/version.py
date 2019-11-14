fe465653f2f84264cca5cad23e7c59c2752b5f53
Clarence <clarence.gardner@salesforce.com>
2019-11-13 20:22:26 -0500

    Test steps

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 2, 135)
-VERSION_STR = '1.2.135'
+VERSION = (1, 2, 138)
+VERSION_STR = '1.2.138'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = 'db8f6f20dc1a0a1c131b83cbad84f205'
+PY_MD5 = '10b192cd90207dce4da2fc53dedbd7ac'
 
-TIMESTAMP = datetime.datetime(2019, 11, 5, 13, 31, 17, 599593)
+TIMESTAMP = datetime.datetime(2019, 11, 13, 20, 3, 6, 16148)
 USER_NAME = 'Clarence'
 USER_EMAIL = 'clarence.gardner@salesforce.com'

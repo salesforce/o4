4283fe349d649f36a6a531922a816324c918f55e
Philip Bergen <pbergen@salesforce.com>
2019-11-21 22:05:06 -0500

    Putting the sync before the resolve fixed the issue of 'b' not getting the new content.

--- o4/version.py
+++ o4/version.py
@@ -1,14 +1,15 @@
+
 # This file is generated, do not modify
 
 import datetime
 
-VERSION = (1, 2, 138)
-VERSION_STR = '1.2.138'
+VERSION = (1, 2, 142)
+VERSION_STR = '1.2.142'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '10b192cd90207dce4da2fc53dedbd7ac'
+PY_MD5 = '9917c3ea1e2641688f8a7185c5bf5a02'
 
-TIMESTAMP = datetime.datetime(2019, 11, 13, 20, 3, 6, 16148)
-USER_NAME = 'Clarence'
-USER_EMAIL = 'clarence.gardner@salesforce.com'
+TIMESTAMP = datetime.datetime(2019, 11, 21, 22, 2, 41, 103360)
+USER_NAME = 'Philip Bergen'
+USER_EMAIL = 'pbergen@salesforce.com'

1b927a32deba578ced3330ff78f26d2a50b348a8
Clarence <clarence.gardner@salesforce.com>
2019-11-27 16:46:54 -0500

    Bump version number for release.

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 2, 142)
-VERSION_STR = '1.2.142'
+VERSION = (1, 2, 143)
+VERSION_STR = '1.2.143'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '9917c3ea1e2641688f8a7185c5bf5a02'
+PY_MD5 = '7a6fd0bfe57cfef0b2d14887f46290e4'
 
-TIMESTAMP = datetime.datetime(2019, 11, 21, 22, 2, 41, 103360)
-USER_NAME = 'Philip Bergen'
-USER_EMAIL = 'pbergen@salesforce.com'
+TIMESTAMP = datetime.datetime(2019, 11, 27, 16, 42, 43, 186111)
+USER_NAME = 'Clarence'
+USER_EMAIL = 'clarence.gardner@salesforce.com'

6e4150509b3f19377434a301690ed042e40f16c1
Clarence Gardner <clarence.gardner@salesforce.com>
2020-02-18 14:55:49 -0500

    Simpler no-problem check.

--- o4/o4.py
+++ o4/o4.py
@@ -1101,8 +1101,10 @@ def o4_status(changelist, depot):
     if all_fnames:
         print("\nFiles with local modifications:")
         print(" (!=Mismatch A=Added D=Deleted M=Modified O=Open R=Renamed)\n")
+    else:
+        print("*** INFO: No problems found :)")
+        return True
 
-    problems = False
     for f in sorted(all_fnames):
         ho = has_open.get(f, {})
         if ho.get('action') == 'move/add':
@@ -1126,10 +1128,6 @@ def o4_status(changelist, depot):
         if m == ' ' and f in has_open:
             m = 'O'
         print(f" {n}{r}{m}  {fn}")
-        problems = True
-
-    if not problems:
-        print("*** INFO: No problems found :)")
     return True
 
 

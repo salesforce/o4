58aca80c1501e3893cdc9bc175546a9b056573dd
Philip Bergen <pbergen@salesforce.com>
2020-02-19 04:27:51 -0500

    Cosmetics.

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 2, 185)
-VERSION_STR = '1.2.185'
+VERSION = (1, 2, 188)
+VERSION_STR = '1.2.188'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '06f4ca7a8aabda4b9203fb197f8d3c0a'
+PY_MD5 = '729f1b1fda0e737fff2c8b38523d974e'
 
-TIMESTAMP = datetime.datetime(2020, 2, 19, 4, 24, 24, 578042)
+TIMESTAMP = datetime.datetime(2020, 2, 19, 4, 27, 39, 913696)
 USER_NAME = 'Philip Bergen'
 USER_EMAIL = 'pbergen@salesforce.com'

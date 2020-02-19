ae2e8764f59f31b60aa902ee8019a5f61751436b
Philip Bergen <pbergen@salesforce.com>
2020-02-19 03:05:02 -0500

    35% reduction in processing time inside gatling/manifold by switching from str to bytes (skips decode/encode).

--- gatling/version.py
+++ gatling/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 1, 23)
-VERSION_STR = '1.1.23'
+VERSION = (1, 1, 35)
+VERSION_STR = '1.1.35'
 PRODUCT = 'gatling/version.py'
 
 REQ_MD5 = '66d2b20c6fa5c95ac17a05d908889c66'
-PY_MD5 = '62273bde40a3b511b729214f91457f9a'
+PY_MD5 = 'c1cb4c63bd786e9c4df6ec0644594904'
 
-TIMESTAMP = datetime.datetime(2020, 2, 19, 1, 34, 33, 945519)
+TIMESTAMP = datetime.datetime(2020, 2, 19, 3, 0, 9, 782358)
 USER_NAME = 'Philip Bergen'
 USER_EMAIL = 'pbergen@salesforce.com'
--- manifold/version.py
+++ manifold/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 1, 24)
-VERSION_STR = '1.1.24'
+VERSION = (1, 1, 40)
+VERSION_STR = '1.1.40'
 PRODUCT = 'manifold/version.py'
 
 REQ_MD5 = '66d2b20c6fa5c95ac17a05d908889c66'
-PY_MD5 = '8eee445be3b4b4371cee5328482a25f5'
+PY_MD5 = '1b26da54c34c9c5dea03f172cbd1cca0'
 
-TIMESTAMP = datetime.datetime(2020, 2, 19, 1, 34, 34, 969509)
+TIMESTAMP = datetime.datetime(2020, 2, 19, 3, 0, 11, 228319)
 USER_NAME = 'Philip Bergen'
 USER_EMAIL = 'pbergen@salesforce.com'

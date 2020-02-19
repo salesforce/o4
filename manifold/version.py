fe183bc06d35746cd8a90d076de983eea013a82a
Philip Bergen <pbergen@salesforce.com>
2020-02-19 01:48:42 -0500

    Circumvent the busy wait that made gatling peg the cpu at 100% after the input ended.

--- manifold/version.py
+++ manifold/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 1, 13)
-VERSION_STR = '1.1.13'
+VERSION = (1, 1, 24)
+VERSION_STR = '1.1.24'
 PRODUCT = 'manifold/version.py'
 
 REQ_MD5 = '66d2b20c6fa5c95ac17a05d908889c66'
-PY_MD5 = 'c964b390c5df7758462e5ff574c586f4'
+PY_MD5 = '8eee445be3b4b4371cee5328482a25f5'
 
-TIMESTAMP = datetime.datetime(2020, 2, 10, 13, 10, 30, 149848)
-USER_NAME = 'Clarence'
-USER_EMAIL = 'clarence.gardner@salesforce.com'
+TIMESTAMP = datetime.datetime(2020, 2, 19, 1, 34, 34, 969509)
+USER_NAME = 'Philip Bergen'
+USER_EMAIL = 'pbergen@salesforce.com'

a993a432da3c0054ba18d9214dbbbed28e824282
Philip Bergen <pbergen@salesforce.com>
2020-02-21 00:34:49 -0500

    Move keep_case down the stack for higher performance.

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 2, 190)
-VERSION_STR = '1.2.190'
+VERSION = (1, 2, 192)
+VERSION_STR = '1.2.192'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = 'ad94e05ce2b8033a4de312e396af11d5'
+PY_MD5 = '5d4ed2e4b4c8fb99120700822b79724b'
 
-TIMESTAMP = datetime.datetime(2020, 2, 19, 17, 59, 46, 177673)
+TIMESTAMP = datetime.datetime(2020, 2, 21, 0, 30, 10, 477740)
 USER_NAME = 'Philip Bergen'
 USER_EMAIL = 'pbergen@salesforce.com'

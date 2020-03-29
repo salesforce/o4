8f9ce67dd3f4803717a31517f93c0c5c5cd46e4a
Clarence <clarence.gardner@salesforce.com>
2020-03-29 17:45:06 -0400

    seed-from forward fstat record only if sync is needed.

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 2, 204)
-VERSION_STR = '1.2.204'
+VERSION = (1, 2, 205)
+VERSION_STR = '1.2.205'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '2cafd00f0adf4774904672a754f422c0'
+PY_MD5 = 'e0f9a7a10afa9594d4690d23aa3aa0de'
 
-TIMESTAMP = datetime.datetime(2020, 3, 26, 20, 39, 16, 723490)
+TIMESTAMP = datetime.datetime(2020, 3, 29, 17, 38, 52, 474769)
 USER_NAME = 'Clarence'
 USER_EMAIL = 'clarence.gardner@salesforce.com'

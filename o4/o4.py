23610b28c63e44257c83c9a842dbc1973af88e76
Clarence <clarence.gardner@salesforce.com>
2020-04-22 21:29:41 -0400

    Add deprecation message for clean --discard

--- o4/o4.py
+++ o4/o4.py
@@ -21,16 +21,17 @@ Option:
                 provide -f. For a faster check from 80% of current changelist,
                 add -q (recommended).
   clean         Clean <path>.
-  <path>        Specify perforce style path, optionally specify "@changelist", if not given, head
-                will be determined. If path is a directory, "/..." is implied.
+  <path>        Specify perforce style path, optionally specify "@changelist";
+                if not given, head will be determined. If path is a directory,
+                "/..." is implied.
                 This path must always be a directory, not a file.
   -s <seed>     Seed sync with files from a path.
   -S <seed>     Old o4 compatibility flag. Do not use, deprecated.
   --resume      Automatically resumes a clean if <path>.o4-bak exists.
-  --discard     Delete the files that should not exist (i.e., don't save them in a separate
-                location).
-  fstat         Stream fstat lines for a [depot] path. Paths can contain changelist in
-                the '<path>@<changelist>' notation.
+  --discard     Delete the files that should not exist (i.e., don't save them
+                in a separate location). DEPRECATED
+  fstat         Stream fstat lines for a [depot] path. Paths can contain
+                changelist in the '<path>@<changelist>' notation.
   --changed <previous>  Only output fstat for changes in (<previous>,<changelist>]
   --drop <fname>  Remove fstat with path listed in <fname>.
   --keep <fname>  Only keep fstat with path listed in <fname>.
@@ -1249,6 +1250,7 @@ def o4_clean(changelist, quick=False, resume=False, discard=False):
     else:
         assert source.endswith('cleaning')
         shutil.rmtree(source)
+        print('*** INFO: The discard option is deprecated', file=sys.stderr)
 
 
 def rm_empty_dirs(root):

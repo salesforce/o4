3c1a34e042bf4b21113888a133805e0a2e8acc08
Clarence Gardner <clarence.gardner@salesforce.com>
2021-03-09 15:09:32 -0500

    version

--- o4/version.py
+++ o4/version.py
@@ -3,13 +3,13 @@
 
 import datetime
 
-VERSION = (1, 5, 1)
-VERSION_STR = '1.5.1'
+VERSION = (1, 5, 2)
+VERSION_STR = '1.5.2'
 PRODUCT = 'o4/version.py'
 
 REQ_MD5 = '43a9b7190217ebf4434beeb068e7a833'
-PY_MD5 = '14ad9ddecbcbbc8b648617c1b5ca8183'
+PY_MD5 = 'ee0ea7d3b5645be4c1bcdfce74618eb8'
 
-TIMESTAMP = datetime.datetime(2021, 3, 5, 10, 1, 27, 352798)
-USER_NAME = 'Philip Bergen'
-USER_EMAIL = 'pbergen@salesforce.com'
+TIMESTAMP = datetime.datetime(2021, 3, 9, 15, 9, 1, 916319)
+USER_NAME = 'Clarence Gardner'
+USER_EMAIL = 'clarence.gardner@salesforce.com'
